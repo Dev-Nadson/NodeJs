d44c7917017d9cde4eeb2113b58c3e20f7587dfb
Dev-Nadson <nadson.pros@gmail.com>
2025-07-30 23:40:37 -0300

    Feat: 'Not Found 404' transferido para o routesHandler

--- api/src/server.js
+++ api/src/server.js
@@ -1,19 +1,10 @@
 import http from "node:http"
 import { jsonBodyHandler } from "./middlewares/jsonBodyHandler.js"
+import { routeHandler } from "./middlewares/routesHandler.js"
+
 const server = http.createServer(async (request, response) => {
-    const { method, url } = request
-
     await jsonBodyHandler(request, response)
-    if (method === "GET" && url === "/products") {
-        console.log(response.body)
-        return response.end("Lista de Produtos")
-    }
-
-    if (method === "POST" && url === "/products") {
-        console.log(response.body)
-        return response.end(JSON.stringify(request.body))
-    }
-    return response.writeHead(404).end("Not Found")
+    routeHandler(request, response)
 })
 
 server.listen(3333)
