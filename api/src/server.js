791bc451de00c6a3743a696c0919536068c1fa88
Dev-Nadson <nadson.pros@gmail.com>
2025-07-30 23:18:24 -0300

    Feat: Middleware adicionado

--- api/src/server.js
+++ api/src/server.js
@@ -1,14 +1,17 @@
 import http from "node:http"
-
-const server = http.createServer((request, response) => {
+import { jsonBodyHandler } from "./middlewares/jsonBodyHandler.js"
+const server = http.createServer(async (request, response) => {
     const { method, url } = request
 
+    await jsonBodyHandler(request, response)
     if (method === "GET" && url === "/products") {
+        console.log(response.body)
         return response.end("Lista de Produtos")
     }
 
     if (method === "POST" && url === "/products") {
-        return response.end("Lista de Produtos")
+        console.log(response.body)
+        return response.end(JSON.stringify(request.body))
     }
     return response.writeHead(404).end("Not Found")
 })
