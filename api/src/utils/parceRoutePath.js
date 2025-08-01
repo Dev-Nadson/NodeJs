3717bdde16743df3dfdfaa898b4667cec26dee17
Dev-Nadson <nadson.pros@gmail.com>
2025-07-31 23:09:08 -0300

    Feat: Commit Inicial

---
+++ api/src/utils/parceRoutePath.js
@@ -0,0 +1,8 @@
+export function parseRoutePath(path) {
+    const routeParametersRegex = /:([a-zA-Z]+)/g
+
+    const params = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9_]+)")
+    const pathRegex = new RegExp(params)
+
+    return pathRegex
+}
