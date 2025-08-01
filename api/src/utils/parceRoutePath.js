fcf033d1fb4e8ec62c33a5b39457f8b71a7763ee
Dev-Nadson <nadson.pros@gmail.com>
2025-08-01 00:10:54 -0300

    Feat: request de parâmetros nomeados adicionados

--- api/src/utils/parceRoutePath.js
+++ api/src/utils/parceRoutePath.js
@@ -2,7 +2,8 @@ export function parseRoutePath(path) {
     const routeParametersRegex = /:([a-zA-Z]+)/g
 
     const params = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9_]+)")
-    const pathRegex = new RegExp(params)
+
+    const pathRegex = new RegExp(`${params}(?<query>\\?(.*))?$`)
 
     return pathRegex
 }
